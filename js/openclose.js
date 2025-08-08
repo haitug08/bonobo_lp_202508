bbaae019f8d973831ca6fcfc3a3d8209d9fcfa2e
haitug08 <haitug08@gmail.com>
2025-08-08 12:14:39 +0900

    Update openclose.js

--- js/openclose.js
+++ js/openclose.js
@@ -165,27 +165,3 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 });
-
-
-const swiper = new Swiper(".swiper", {
-  pagination: {
-    el: ".swiper-pagination"
-  },
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "2",
-
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: true
-  },
-
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
-  }
-});
